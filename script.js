c31ad24e83a63218bbffd0f0fe69b7c17609b514
durgeshceps <101636946+durgeshceps@users.noreply.github.com>
2024-08-27 20:52:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
+
+audioElement.play();
